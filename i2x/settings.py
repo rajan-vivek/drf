fbba7f58c2b54e383ec58b3d573c3ca8268f9016
vivek <teknomind@gmail.com>
2017-05-21 20:24:17 +0530

    STATIC_ROOT added which was removed earlier

--- i2x/settings.py
+++ i2x/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'i2x/static')
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
